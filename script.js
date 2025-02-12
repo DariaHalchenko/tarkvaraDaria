df472d7b2eee342498e2e2ffe7e3257d9ef50c07
Opilane TTHK <opilane@tthk.ee>
2025-02-12 13:07:15 +0200

    Reset nupp on lisatud

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById("colorbtn").addEventListener("click", function() {
     const colors = ["#FF5733", "#33FF57", "#3357FF", "#F0DB4F", "#8E44AD"];
     document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 });
+
+
+//Ülesanne üks funktsioon:
+document.getElementById("resetbtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = "white";
+});
