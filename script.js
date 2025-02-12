5ff026be9bb2b20bd7d75c3e5831b926df7a1cac
BogdanViblyy <opilane@tthk.ee>
2025-02-12 13:43:25 +0200

    Parandasin kommentarid ja uuendasin koodi. Nüüd päise värv muutub koos taustaga.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+//Ülesanne kolmP funktsioon:
 document.getElementById("colorbtn").addEventListener("click", function() {
-    const colors = ["#FF5733", "#33FF57", "#3357FF", "#F0DB4F", "#8E44AD"];
-    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    const colorMap = new Map([
+        ["#FF5733", "#3357FF"],
+        ["#33FF57", "#8E44AD"],
+        ["#3357FF", "#FF5733"],
+        ["#F0DB4F", "#FF00A6"],
+        ["#8E44AD", "#33FF57"],
+        ["#F0DB4F", "#FF00A6"]
+    ]);
+
+    const colors = Array.from(colorMap.keys());
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    document.body.style.backgroundColor = randomColor;
+
+    const h1 = document.querySelector("h1");
+    h1.style.color = colorMap.get(randomColor);
 });
 
 
-//Ülesanne üks funktsioon:
+//Ülesanne kaks funktsioon:
 document.getElementById("resetbtn").addEventListener("click", function() {
+    const h1 = document.querySelector("h1");
     document.body.style.backgroundColor = "white";
+    h1.style.color = "black";
 });
