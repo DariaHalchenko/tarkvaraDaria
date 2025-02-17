a066a0d49937d271e5ff65dbb1e86ce275c7e69d
bogdanviblyy <bogdan.viblyy@icloud.com>
2025-02-17 12:20:29 +0200

    Tegisin script ülesanneks nr 1

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById("colorbtn").addEventListener("click", function() {
     const colors = ["#FF5733", "#33FF57", "#3357FF", "#F0DB4F", "#8E44AD"];
     document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 });
+const colorSelect = document.getElementById("color-select");
+colorSelect.addEventListener("change", function() {
+    document.body.style.backgroundColor = this.value;
+});
